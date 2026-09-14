55419ac8d3accb984a5427adc3ba5a1f7cff1d60
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-09-14 09:59:20 -0300

    Atualiza URL exata do Apps Script na finalização

--- api/project-finalize.js
+++ api/project-finalize.js
@@ -3,7 +3,7 @@ export const config = {
 };
 
 const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzc1AwswaQnSv4PGGvVGqH-8KebR-kmnFIBxygdeKEeVGGULSvxFT1DCOTiberXi7pQ/exec";
+  "https://script.google.com/macros/s/AKfycbzwcpKcDPNfD05qffeShJ5ldgGQ34nx2oaonkgyfeltZk5-lQNnf5VdinxGp9HkLFj5/exec";
 
 const SUPABASE_URL = "https://omgjbafqukpzdhhpdlaa.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
