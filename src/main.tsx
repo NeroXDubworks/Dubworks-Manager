cbc5bcf7a82c0b78942cba79cd3f026e36dc4f2d
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-09-14 11:41:24 -0300

    Usa bootstrap V2 para criação dos formulários

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./lib/driveFormsBootstrap";
+import "./lib/driveFormsBootstrapV2";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
