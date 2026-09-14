f6b855cf7910fbac4468f28e0afda798f20c4c29
NeroXDubworks <Lorrane.eugenio@outlook.com>
2026-09-14 09:44:52 -0300

    Corrige URL exata da implantação do Apps Script

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -2,7 +2,7 @@ import type { ChavePermissao } from "../types";
 
 export const LOGO_URL = "/Logo_dubworks.png";
 export const FORMS_COPIER_URL =
-  "https://script.google.com/macros/s/AKfycbzc1AwswaQnSv4PGGyVGqH-8KebR-kmnFIBxygdeKEeVGGULSvxFT1DCOTiberXi7pQ/exec";
+  "https://script.google.com/macros/s/AKfycbzc1AwswaQnSv4PGGvVGqH-8KebR-kmnFIBxygdeKEeVGGULSvxFT1DCOTiberXi7pQ/exec";
 
 export const permissoesDisponiveis: { chave: ChavePermissao; label: string }[] = [
   { chave: "acesso_projetos", label: "Projetos" },
